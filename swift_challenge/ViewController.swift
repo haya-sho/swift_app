0e3156d2a6f5aba3679f57c14e6be4de83542b1e
haya-sho <shoko_hasegawa@mail.toyota.co.jp>
2023-06-12 20:09:49 +0900

    3rdCommit

--- swift_challenge/ViewController.swift
+++ swift_challenge/ViewController.swift
@@ -8,16 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        let text = UILabel(frame: CGRect(x: 150, y: 150, width: 300, height: 100))
-//        text.text = "hello world!"
-//        self.view.addSubview(text)
     }
-
-
+    
+    @IBAction func plusMoney(_ sender: Any) {
+        
+        let vc = plusMoneyViewController()
+                navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+    @IBAction func minusMoney(_ sender: Any) {
+        let vc = minusMoneyViewController()
+                navigationController?.pushViewController(vc, animated: true)
+        
+     
+    }
+    
+    
 }
-
